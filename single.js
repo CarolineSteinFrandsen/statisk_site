bbceda00a2cc76a5272d357f146831d46c6f9e48
CarolineSteinFrandsen <cafr0002@stud.kea.dk>
2025-02-04 15:44:04 +0100

    done

--- single.js
+++ single.js
@@ -8,15 +8,22 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${myproduct}`)
 
 function showProduct(data) {
   product_container.innerHTML = `
-   <div>
+             <div class= "billede_productsite">
                 <img class="img_productsite" src="https://kea-alt-del.dk/t7/images/webp/640/${myproduct}.webp" alt="t-shirt">
+                  <div class="udsalgs_boks2 ${!data.discount && "hide"}">${data.discount}%</div>
+                   <div class="soldout_boks2 ${!data.soldout && "hide"}">Udsolgt</div>
             </div>
-
+            
+       
             <div>
+             
                 <div>
-                    <h2 class="h2_product">NIKE</h2>
+                    <h2 class="h2_product">${data.brandname}</h2>
                     <p class="product_text"> <b>${data.productdisplayname}</b></p>
-                    <p class="pris_product">Pris: ${data.price} kr.</p>
+                     <p class="pris_product ${!data.discount && "hide"}"> Nypris: ${Math.round(data.price * (1 - data.discount / 100))} kr.</p>
+                    <p class="pris_product_før">Pris: ${data.price} kr.</p>
+                   
+
                 </div>
 
                  <div>
@@ -27,6 +34,8 @@ function showProduct(data) {
                 <div>
                     <p class="product_farve"> Color: Blå</p>
                 </div>
+                
+                
                 <div class="product-options">
                     <select class="size-selector">
                         <option selected disabled>Vælg størrelse</option>
